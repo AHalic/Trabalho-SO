c3a58db7f93efe777c9c353facb5de4f91820332
bea <beamaia.work@gmail.com>
2021-04-17 20:13:18 -0300

    add functions

--- messages.c
+++ messages.c
@@ -4,3 +4,21 @@
 void show_command_line() {
     printf("vsh > ");
 }
+
+char** read_command_line(int n_process) {
+    n_process = 0;
+    
+}
+
+void handle_sigusr() {
+    printf("\n"
+            "              _  _\n"
+            "    _ _      (0)(0)-._  _.-'^^'^^'^^'^^'^^'--.\n"
+            "   (.(.)----'`        ^^'                /^   ^^-._\n"
+            "    (    `                 \\             |    _    ^^-._\n"
+            "    VvvvvvvVv~~`__,/.._>  /:/:/:/:/:/:/:/\\  (_..,______^^-.\n"
+            "     `^^^^^^^^`/  /   /  /`^^^^^^^^^>^^>^`>  >        _`)  )\n"
+            "              (((`   (((`          (((`  (((`        `'--'^\n"
+            "I feel weird...\n");
+}
+
--- messages.h
+++ messages.h
@@ -1,6 +1,11 @@
-#ifdef MESSAGES_H
+#ifndef MESSAGES_H
 #define MESSAGES_H
 
 void show_command_line();
+
+char** read_command_line(int n_process);
+
+void handle_sigusr();
+
 
 #endif
