827e34a570097259271bfb9d297bdffae46dd1fc
AHalic <sophie.gama@aluno.ufes.br>
2021-04-17 17:55:22 -0300

    Getline funcionando

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
@@ -11,17 +12,32 @@
 int main() {
     // Configura a mascara de sinais bloqueados 
     // DUVIDA SE PRECISA
-    sigset_t shell_mask;
-    sigemptyset(&shell_mask);
-    sigaddset(&shell_mask, SIGINT);
-    sigprocmask(SIG_SETMASK, &shell_mask, NULL);
+    // sigset_t shell_mask;
+    // sigemptyset(&shell_mask);
+    // sigaddset(&shell_mask, SIGINT);
+    // sigprocmask(SIG_SETMASK, &shell_mask, NULL);
 
-    char *comandos;
+
+
+
+    //le o resto do arquivo e retorna n
     char* line_buf = NULL;
+    size_t line_buf_size = 0;
+    int line_count = 0;
 
-    while(true) {
+    // ler primeira linha fora do loop
+    show_command_line();
+    getline(&line_buf, &line_buf_size, stdin);
+
+    // Retirando o caracter \n ou \r do fim da string (lido pelo getline)
+    line_buf[strcspn(line_buf, "\r\n")] = 0; 
+
+    while (strcmp(line_buf, "armageddon") != 0) {
         show_command_line();
-        getline(&line_buf, NULL, stdin);  // n funcionou
-        
+        getline(&line_buf, &line_buf_size, stdin);        
+        line_buf[strcspn(line_buf, "\r\n")] = 0; 
     }
+    
+    free(line_buf);
+
 }
