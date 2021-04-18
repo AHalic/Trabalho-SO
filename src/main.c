c831f3ccac1119e04a3e4a245429ac54c1305f23
bea <beamaia.work@gmail.com>
2021-04-18 14:06:41 -0300

    retira comentarios

--- src/main.c
+++ src/main.c
@@ -6,53 +6,30 @@
 
 #include "vsh_io.h"
 #include "vsh_handler.h"
+#include "vsh_commands.h"
 
 #define true 1
 #define false 0
 
 int main(int argc, char* argv[]) {
-    // printf("arg0: %s\n\n", argv[0]);
     // Configura a mascara de sinais bloqueados (SIGINT)
-    // sigset_t shell_mask;
-    // sigemptyset(&shell_mask);
-    // sigaddset(&shell_mask, SIGINT);
-    // sigprocmask(SIG_SETMASK, &shell_mask, NULL);
+    // configure_mask();
 
-    //le o resto do arquivo e retorna n
-    // char* line_buf = NULL, *exec, *args, *process1, *process2;
-    char** processes;
-    // size_t line_buf_size = 0;
+    char** commands_vector;
     int n_arguments = 0;
 
-    // ler primeira linha fora do loop
+    // ler primeira linha antes do loop
     do {
         show_command_line();
-        processes = read_command_line(&n_arguments);
+        commands_vector = read_command_line(&n_arguments);
 
         if (n_arguments == 0) continue;
 
-        if (!strcmp(processes[0], "armageddon"))
-            return 0;
+        if (quit_shell(commands_vector[0])) return 0;
         
         for (int i = 0; i < n_arguments; i++) {
-            // printf("command: %s\n", processes[i]);
-            execute_command(processes[i], n_arguments-1);
+            execute_command(commands_vector[i], n_arguments-1);
         }
-    } while (1);
+
+    } while (true);
 }
-
-/*
-
-vsh > ./main a b c | ls
-
-processos = "./main a b c", "ls"
-...
-// coloca um \0 no final?
-processo = "./main a b c"
-executavel = strtok(processo, " ");
-argumentos = strtok(NULL, "\0");
-
-if (execve(executavel, arg, envp) == -1)
-    perror("Could not execve");
-
-*/
