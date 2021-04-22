e3867784bb283cc713fe0ded6606e3cd0e33648a
bea <beamaia.work@gmail.com>
2021-04-21 22:48:57 -0300

    adiciona handlers para SIGINT, SIGQUIT, SIGTSTP

--- src/vsh_handler.c
+++ src/vsh_handler.c
@@ -1,6 +1,17 @@
 #include "vsh_handler.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void configure_sigaction_fg() {
+    
+}
+
+void configure_sigaction_bg() {
+
+}
 
 void handle_sigusr_vsh() {
     printf("\n"
@@ -14,3 +25,25 @@ void handle_sigusr_vsh() {
             "I feel weird...\n");
 }
 
+void handle_sigusr_sick() {
+    printf("PID: %d, PPID: %d\n", getpid(), getppid());
+    exit(1);
+}
+
+void handle_sigusr_vaccinated() {
+    printf("I am dad %d\n", getpid());
+    kill(getppid(), SIGUSR1);
+    raise(SIGTERM);
+}
+
+void handle_sigint() {
+    return;
+}
+
+void handle_sigquit() {
+    return;
+}
+
+void handle_sigtstp() {
+    return;
+}
