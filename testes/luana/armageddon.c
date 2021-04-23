daf20e66b3809dcb2533dbb85ecd2d4e68365d67
Luana Gabriele de Sousa Costa <luanagabrielescosta@gmail.com>
2021-04-23 18:31:30 -0300

    testes iniciais do caos

---
+++ testes/luana/armageddon.c
@@ -0,0 +1,11 @@
+// Arquivo de testes do comando Armageddon
+
+// includes:
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
+
+// kill( all pids, SIG_TERM );
+// thats all folks
