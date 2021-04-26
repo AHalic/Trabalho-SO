64c93ceb2a19f5ea24e8d0a5b342ab9ab555dd65
bea <beamaia.work@gmail.com>
2021-04-26 14:44:39 -0300

    adiciona handlers de sinais

--- src/vsh_commands.c
+++ src/vsh_commands.c
@@ -13,6 +13,16 @@
 #include "vsh_handler.h"
 
 void configure_signals_vsh() {
+    struct sigaction sa_dft;
+    sa_dft.sa_flags =  SA_RESTART;
+    sa_dft.sa_handler = SIG_DFL;
+
+    sigaction(SIGINT, &sa_dft, NULL);
+    sigaction(SIGQUIT, &sa_dft, NULL);
+    sigaction(SIGTSTP, &sa_dft, NULL);  
+}
+
+void configure_signals_vsh_sigusr() {
     struct sigaction block_signal;
     sigset_t mask;
 
@@ -24,23 +34,32 @@ void configure_signals_vsh() {
     block_signal.sa_mask = mask;
     block_signal.sa_handler = &handle_sigusr_vsh;
     block_signal.sa_flags = SA_RESTART;
-    // sa_sigint.sa_flags = sa_sigquit.sa_flags = sa_sigtstp.sa_flags = sa_sigusr.sa_flags = SA_RESTART;
 
-    // sa_sigint.sa_handler = &handle_sigint;
-    // sa_sigquit.sa_handler = &handle_sigquit;
-    // sa_sigtstp.sa_handler = &handle_sigtstp;
-    // sa_sigusr.sa_handler = &handle_sigusr_vsh;
-    
-    // sigaction(SIGINT, &sa_sigint, NULL);
-    // sigaction(SIGQUIT, &sa_sigquit, NULL);
-    // sigaction(SIGTSTP, &sa_sigtstp, NULL);
     sigaction(SIGUSR1, &block_signal, NULL);
     sigaction(SIGUSR2, &block_signal, NULL);
 }
 
+void configure_signals_vsh_ignore() {  
+    struct sigaction sa_ign;
+    sa_ign.sa_flags = SA_RESTART;
+    sa_ign.sa_handler = SIG_IGN;
 
+    sigaction(SIGINT, &sa_ign, NULL);
+    sigaction(SIGQUIT, &sa_ign, NULL);
+    sigaction(SIGTSTP, &sa_ign, NULL);
+}
 
 void configure_signals_fg() {
+    struct sigaction sa_dft, sa_sigusr;
+    sa_dft.sa_flags =  sa_sigusr.sa_flags = SA_RESTART;
+
+    sa_dft.sa_handler = SIG_DFL;
+    sa_sigusr.sa_handler = SIG_IGN;
+    
+    sigaction(SIGINT, &sa_dft, NULL);
+    sigaction(SIGQUIT, &sa_dft, NULL);
+    sigaction(SIGTSTP, &sa_dft, NULL);
+
     struct sigaction block_signal;
     sigset_t mask;
 
@@ -55,24 +74,14 @@ void configure_signals_fg() {
     
     sigaction(SIGUSR1, &block_signal, NULL);
     sigaction(SIGUSR2, &block_signal, NULL);
-
-    // struct sigaction sa_dft, sa_sigusr;
-    // sa_dft.sa_flags =  sa_sigusr.sa_flags = SA_RESTART;
-
-    // sa_dft.sa_handler = SIG_DFL;
-    // sa_sigusr.sa_handler = SIG_IGN;
-    
-    // sigaction(SIGINT, &sa_dft, NULL);
-    // sigaction(SIGQUIT, &sa_dft, NULL);
-    // sigaction(SIGTSTP, &sa_dft, NULL);
 }
 
 int quit_shell(char* command, pid_t* pids, int n_commands) {
     // Ve se existe a substring armageddon
     if (strstr(command, "armageddon")) {
-        // for(int i = 0; i < n_commands; i++){
-        //     kill(pids[i], SIGTERM);
-        // }
+        for(int i = 0; i < n_commands; i++){
+            kill(pids[i], SIGKILL);
+        }
         
         // pid_t pid;
         // int status;
@@ -94,7 +103,7 @@ int quit_shell(char* command, pid_t* pids, int n_commands) {
     return 0;
 }
 
-int destroy_zombies(char* command, int pid){
+int destroy_zombies(char* command){
     static int errors = 0; // filhos com erro
     static int total = 0; // filhos que terminaram
     float percent = 0; // percentual
@@ -102,20 +111,16 @@ int destroy_zombies(char* command, int pid){
 
     if (strstr(command, "liberamoita")){
          // loop nos filhos mortos
-
-        if (pid) {
-            while(waitpid(0, NULL, WUNTRACED) > -1);
-            // nao funcionou isso
-            // a minha ideia era q podiamos usar pra wait os filhos
-        }
-        else {
-            while((pid = waitpid(-1, &status, WNOHANG)) > -1) {
-            // atualiza contadorif (WIFEXITED(status) && WEXITSTATUS(status) != EXIT_SUCCESS) {
+        while(waitpid(-1, &status, WNOHANG) > -1) {
+            // atualiza contador
+            if (WIFEXITED(status) && WEXITSTATUS(status) != EXIT_SUCCESS){
+                break;
             }
         }   
-        
-        return 1;
     }
+        // print teste
+        free(command);
+        return 1;
 
     return 0;
 }
--- src/vsh_commands.h
+++ src/vsh_commands.h
@@ -4,11 +4,15 @@
 
 void configure_signals_vsh();
 
+void configure_signals_vsh_sigusr();
+
 void configure_signals_fg();
+
+void configure_signals_vsh_ignore();
 
 int quit_shell(char* command, pid_t* pids, int n_commands);
 
-int destroy_zombies(char* command, int pid);
+int destroy_zombies(char* command);
 
 void destroy_commands(char** commands, int n_commands);
 
