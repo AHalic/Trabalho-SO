dc610bd899a1e36e6a1dcb7d7cb74bd59159ac8c
bea <beamaia.work@gmail.com>
2021-04-27 14:25:37 -0300

    retira bibliotecas padroes

--- src/main.c
+++ src/main.c
@@ -1,10 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/wait.h>
-
 #include "vsh_io.h"
 #include "vsh_handler.h"
 #include "vsh_commands.h"
@@ -13,46 +6,54 @@
 #include "vsh_list.h"
 
 #define true 1
-#define false 0
 
 int main(int argc, char* argv[]) {
-    // Configura a mascara de sinais
+    // configura a mascara de sinais para os SIGUSRs
     configure_signals_vsh_sigusr();
 
+    // cria lista vazia para gid
     SessionList* gid_list = init_empty_gid_list();
+
     int n_commands = 0, session;
 
     // ler primeira linha antes do loop
     do {
+        // mostra linha de comando e recebe comandos
         show_command_line();
         char** commands_vector = read_command_line(&n_commands);
 
+        // se nao tiver comandos, volta para o inicio
         if (n_commands == 0) continue;
 
+        // se tiver apenas 1 comando, verificar se sao comandos da shell
         if(n_commands == 1){
-            if(destroy_zombies(commands_vector[0], 1))
+            if(destroy_zombies(commands_vector[0], 1)) {
+                destroy_commands(commands_vector, 1);
                 continue;
+            }
             if(quit_shell(commands_vector[0], gid_list)) {
-                printf("oi\n");
-                usleep(100);
                 destroy_list(gid_list);
                 free(commands_vector);
                 break;
             }
         }
 
+        // executa os programas/processos e obtem o gid da sessao
         session = execute_programs(n_commands, commands_vector);
-        printf("s: %d\n", session);
-        SessionList* gid_list = add_gid(gid_list, session);
+        gid_list = add_gid(gid_list, session);
+
+        // reconfigura os sinais SIGINT, SIGQUIT, SIGTSTP para default
         configure_signals_vsh();
 
-        // if (n_commands > 1) {
-        //     destroy_zombies("liberamoita", 1);
-        // }
+        // libera zombies criados em processos bg
+        if (n_commands > 1) {
+            destroy_zombies("liberamoita", 1);
+        }
         
+        // libera o vetor de comandos
         destroy_commands(commands_vector, n_commands);
 
-    } while (true);
+    } while(true);
 
     return 0;    
 }
