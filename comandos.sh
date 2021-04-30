4afc3ee8acc257f49dd9305dc9a041bf13b326f9
bea <beamaia.work@gmail.com>
2021-04-30 19:19:36 -0300

    comandos

---
+++ comandos.sh
@@ -0,0 +1,10 @@
+python3 testes/counter.py 3 | cat
+echo 10
+python3 testes/counter.py 2 | cat
+echo 10
+python3 testes/counter.py 2 | cat
+echo 10
+python3 testes/counter.py 3 | cat
+echo 20 | cat
+echo 20 | cat
+ps aux | grep python3
