5507e9b828708dd40d18e40f9548a1158d13cee6
bea <beamaia.work@gmail.com>
2021-05-01 15:24:16 -0300

    muda forma de parametros

--- src/vsh_execute.h
+++ src/vsh_execute.h
@@ -4,10 +4,10 @@
 /**
  * Executa um processo. Se for um processo bg, eh importante saber sua posicao na linha de comando
  * e a matriz de pipes. 
- * @param {char* command}  String do comando que sera executado (Formato: exce argv)
- * @param {int bg} 0 se for um comando fg, bg > 0 se for bg. Eh o numero de comandos - 1;
- * @param {int* fd[2]} Matriz de pipes com a quantidade de pipes igual ao parametro bg.
- * @param {int pos}
+ * @param {char*}command  String do comando que sera executado (Formato: exce argv)
+ * @param {int}bg 0 se for um comando fg, bg > 0 se for bg. Eh o numero de comandos - 1;
+ * @param {int*}fd[2] Matriz de pipes com a quantidade de pipes igual ao parametro bg.
+ * @param {int}pos
  * 
  * @return Numero do processo do filho.
  */
@@ -16,8 +16,8 @@ int execute_command(char* command, int bg, int fd[bg][2], int pos);
 
 /**
  * Executa todos os processos dado um numero de comando e um vetor de string. 
- * @param {int n_commands} Quandtidade de numero de comandos que devem ser executados;
- * @param {char** commands_vector} Vetor de Strings em que cada String eh um executavel + seus argumentos.
+ * @param {int}n_commands Quandtidade de numero de comandos que devem ser executados;
+ * @param {char**}commands_vector Vetor de Strings em que cada String eh um executavel + seus argumentos.
  * 
  * @return Numero da sessao dos processos.
  */
